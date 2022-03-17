75126ff955dcc2ae100236dc36872eebfde11a12
Blake Eriks <eblake98@vt.edu>
2022-03-17 15:39:22 -0700

    Add cooldownTime, zombie level and readyTime

--- lesson-3/zombieFactory.sol
+++ lesson-3/zombieFactory.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.5.0;
+pragma solidity >=0.5.0 <0.6.0;
 
 import "./ownable.sol";
 
@@ -8,10 +8,14 @@ contract ZombieFactory is Ownable {
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+    // 1. Define `cooldownTime` here
+    uint cooldownTime = 1 days
 
     struct Zombie {
         string name;
         uint dna;
+        uint32 level;
+        uint32 readyTime;
     }
 
     Zombie[] public zombies;
@@ -20,7 +24,8 @@ contract ZombieFactory is Ownable {
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string memory _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        // 2. Update the following line:
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
